68de97d1bbf2b238abd0d3eaf438c16f9971e805
Taemoon Bang <ba178711@gmail.com>
2022-04-19 13:18:03 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+      $(document).ready(function(){
+        /*웹페이지 열었을 때*/
+        $("#triangle_close").show();
+        $("#triangle_open").hide();
+
+        /*close을 클릭했을 때 open를 보여줌*/
+        $("#triangle_close").click(function(){
+            $("#triangle_close").hide();
+            $("#triangle_open").show();
+            $("#bars").hide();
+        });
+
+        /*open를 클릭했을 때 close을 보여줌*/
+        $("#triangle_open").click(function(){
+            $("#triangle_close").show();
+            $("#triangle_open").hide();
+            $("#bars").show();
+        });
+
+        $("#bars").click(function(){
+            $("#bars").hide();
+            $("#triangle_close").hide();
+            $("#triangle_open").show();
+        })
+    });
+
+/*sidebar*/
+function toggleSidebar(ref){
+  document.getElementById("sidebar").classList.toggle('active');
+}
+  
+
+
+//input 입력 창을 지워줌
+
+function clearInput(){
+  let text = document.getElementsByClassName('input_text');
+
+for(let i=0; i<text.length; i++){
+	  text[i].value = '';
+}
+}
